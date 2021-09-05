5aeb850d6185b95ad5c8c4671f4d675939237e80
Tyler <tylerking885@knights.ucf.edu>
2021-09-05 18:22:15 -0400

    Comment Revision

--- src/main/java/Solution03.java
+++ src/main/java/Solution03.java
@@ -1,19 +1,19 @@
 /*
- * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * UCF COP3330 Fall 2021 Assignment 1 Solution 03
  * Copyright 2021 Tyler King
  */
 
-import java.util.Scanner;
+import java.util.Scanner;                                   // Import declaration for class scanner;
 
 public class Solution03 {
-    public static void main(String[] args) {
-        String quote, author;
-        Scanner input = new Scanner(System.in);
-        System.out.print("What is the quote? ");
-        quote = input.nextLine();
-        quote = "\"" + quote + "\"";
-        System.out.print("Who said it? ");
-        author = input.nextLine();
-        System.out.print(author + " says, " + quote);
+    public static void main(String[] args) {                // Main method;
+        String quote, author;                               // Declare String variables (quote and author);
+        Scanner input = new Scanner(System.in);             // Enable program to read User data;
+        System.out.print("What is the quote? ");            // Prompt user for a string variable (a quote);
+        quote = input.nextLine();                           // Set string variable quote to user's input;
+        quote = "\"" + quote + "\"";                        // Update user's quote with quotation marks;
+        System.out.print("Who said it? ");                  // Prompt user for the author of the quote;
+        author = input.nextLine();                          // Set string variable author to user's input;
+        System.out.print(author + " says, " + quote);       // Print out author and quote in a message to user;
     }
 }
